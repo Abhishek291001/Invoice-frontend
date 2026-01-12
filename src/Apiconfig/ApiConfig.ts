dc9386bdcef759216749de9423a08a7ee2c4ffb1
TAS3-maker <dm@techarchsoftwares.com>
2026-01-12 17:15:19 +0530

    .env removed from frontend

--- src/Apiconfig/ApiConfig.ts
+++ src/Apiconfig/ApiConfig.ts
@@ -1,5 +1,7 @@
 
  //const API_BASE_URL = "https://hintswork-backend.vercel.app"; 
- const API_BASE_URL = "http://localhost:5000"; 
+//  const API_BASE_URL = "http://localhost:5000"; 
+// const API_BASE_URL = "https://task-invoicebackend.onrender.com"; 
+const API_BASE_URL = import.meta.env.VITE_API_URL; 
 
 export default API_BASE_URL;
